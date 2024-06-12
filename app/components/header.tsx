8bc8457b5c96b441abc99a3852eb42b72babd3ee
Luiz Redig <luiz.redig@silcom.com.br>
2024-06-12 10:26:08 -0300

    feat: add header component

---
+++ app/components/header.tsx
@@ -0,0 +1,46 @@
+import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { Card } from "./ui/card";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+
+const Header = () => {
+  return (
+    <>
+      <Card className="flex h-[85px] w-full flex-row items-center justify-between px-8 md:px-32">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size={"icon"} variant={"outline"}>
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side={"left"}>
+            <SheetHeader>Menu</SheetHeader>
+          </SheetContent>
+        </Sheet>
+
+        <h1 className="flex select-none flex-row gap-1 text-lg font-semibold">
+          <span className="bg-gradient-to-r from-primary to-[#8162FF] bg-clip-text font-bold text-transparent">
+            NEXT
+          </span>
+
+          <span>Eccomerce</span>
+        </h1>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size={"icon"} variant={"outline"}>
+              <ShoppingCartIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side={"right"}>
+            <SheetHeader>Your cart</SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </Card>
+    </>
+  );
+};
+
+export default Header;
