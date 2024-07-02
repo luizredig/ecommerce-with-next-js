c4a9de8016c9b1fc806d24a8195aca319f8ac3e1
redig <luizredig@gmail.com>
2024-07-02 13:25:00 -0300

    feat: add the first mobile and desktop banner on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Image
+          src={"/mobile-first-banner.png"}
+          alt="Até 55% de desconto só este mês."
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="flex h-auto w-full px-5 py-7 sm:hidden"
+        />
+
+        <Image
+          src={"/desktop-first-banner.png"}
+          alt="Ofertas imperdíveis! Até 55% de desconto só este mês."
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="hidden h-auto w-full sm:flex"
+        />
+      </div>
+    </>
+  );
 }
