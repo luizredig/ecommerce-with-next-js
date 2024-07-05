266bd039ac1faf20705570694c395e90b9215e5c
redig <luizredig@gmail.com>
2024-07-05 13:30:37 -0300

    feat: add Banner component for displaying promotional banners on the home page

---
+++ app/components/banner.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { cn } from "../lib/utils";
+
+interface BannerProps {
+  src: string;
+  alt: string;
+  className?: string;
+}
+
+const Banner = ({ src, alt, className }: BannerProps) => {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={0}
+      height={0}
+      sizes="100vw"
+      priority
+      className={cn(className)}
+    />
+  );
+};
+
+export default Banner;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { prismaClient } from "./lib/prisma";
 import CategoryItem from "./components/categoryItem";
 import Section from "./components/section";
+import Banner from "./components/banner";
 
 const Home = async () => {
   const categories = await prismaClient.category.findMany({});
@@ -18,23 +19,15 @@ const Home = async () => {
   return (
     <>
       <div>
-        <Image
+        <Banner
           src={"/mobile-first-banner.png"}
           alt="Até 55% de desconto só este mês."
-          width={0}
-          height={0}
-          sizes="100vw"
-          priority
           className="flex h-auto w-full px-5 py-7 md:hidden"
         />
 
-        <Image
+        <Banner
           src={"/desktop-first-banner.png"}
           alt="Ofertas imperdíveis! Até 55% de desconto só este mês."
-          width={0}
-          height={0}
-          sizes="100vw"
-          priority
           className="hidden h-auto w-full md:flex"
         />
 
