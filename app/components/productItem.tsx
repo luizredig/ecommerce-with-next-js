225406029b4ad0d9fedbd7bba4829c289f6a15e3
redig <luizredig@gmail.com>
2024-07-05 02:57:01 -0300

    feat: add ProductItem component

---
+++ app/components/productItem.tsx
@@ -0,0 +1,69 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import { ArrowDownIcon, PercentIcon } from "lucide-react";
+
+interface ProductItemProps {
+  product: Product;
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <>
+      <div className="flex min-w-[180px] max-w-[180px] flex-col gap-5">
+        {/* Image */}
+        <div className="relative flex min-h-[180px] w-full min-w-[180px] items-center justify-center rounded-lg bg-muted">
+          <Badge className="absolute left-2 top-2 flex max-h-5 flex-row p-2">
+            <ArrowDownIcon size={16} />
+
+            <p>{product.discountPercentage}</p>
+
+            <PercentIcon size={16} />
+          </Badge>
+
+          <Image
+            src={product.imageUrls[0]}
+            alt={product.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            priority
+            className="h-auto w-full max-w-[70%]"
+          />
+        </div>
+
+        {/* Info */}
+        <div>
+          <p className="truncate font-light">{product.name}</p>
+
+          {product.discountPercentage > 0 ? (
+            <div className="flex flex-row items-center gap-1">
+              <p className="font-semibold">
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(Number(product.basePrice))}
+              </p>
+
+              <p className="truncate text-sm text-accent line-through">
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(Number(product.basePrice))}
+              </p>
+            </div>
+          ) : (
+            <p>
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(Number(product.basePrice))}
+            </p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductItem;
