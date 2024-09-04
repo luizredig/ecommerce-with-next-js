5deb262447a59443f3f064493f9d27d1764a8b84
redig <luizredig@gmail.com>
2024-09-04 13:39:12 -0300

    style(category-page): update layout and styling

--- app/(store)/categories/[slug]/page.tsx
+++ app/(store)/categories/[slug]/page.tsx
@@ -19,20 +19,25 @@ const Page = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <>
-      <div className="flex h-full w-full flex-col gap-8 px-5 py-8 md:px-24">
-        <Badge
-          variant={"outline"}
-          className="flex w-fit flex-row items-center justify-center gap-1 border-2 border-primary py-1"
-        >
-          {CATEGORY_ICON[slug as keyof typeof CATEGORY_ICON]}
+      <div className="flex h-full w-full flex-col gap-8 py-8">
+        <div className="flex w-full px-5 md:px-24">
+          <Badge
+            variant={"outline"}
+            className="flex w-fit flex-row items-center justify-center gap-1 border-2 border-primary py-1"
+          >
+            {CATEGORY_ICON[slug as keyof typeof CATEGORY_ICON]}
 
-          <p className="uppercase">{slug}</p>
-        </Badge>
+            <p className="uppercase">{slug}</p>
+          </Badge>
+        </div>
 
-        <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-3 md:gap-8 lg:grid-cols-4">
+        <div className="grid w-full grid-cols-2 grid-rows-3 gap-4 overflow-x-scroll px-5 sm:gap-8 md:flex md:max-h-[286px] md:px-24 [&::-webkit-scrollbar]:hidden">
           {products.map((product) => (
-            <div key={product.id} className="flex md:h-full md:w-full">
-              <ProductItem product={product} />
+            <div
+              key={product.id}
+              className="h-auto max-h-[286px] md:max-w-[180px]"
+            >
+              <ProductItem key={product.id} product={product} />
             </div>
           ))}
         </div>
