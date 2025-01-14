b4b896d8557815e28a49dff8a76f0ab79106a68b
redig <luizredig@gmail.com>
2025-01-14 09:10:34 -0300

    feat(cart): add badge and empty cart scenario to CartSheet

--- app/components/sheets/CartSheet.tsx
+++ app/components/sheets/CartSheet.tsx
@@ -1,8 +1,13 @@
-import { ShoppingCartIcon } from "lucide-react";
+import { ShoppingCartIcon, XIcon } from "lucide-react";
+import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "../ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet";
+import { useState } from "react";
+import { Product } from "@prisma/client";
 
 const CartSheet = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -11,8 +16,36 @@ const CartSheet = () => {
         </Button>
       </SheetTrigger>
 
-      <SheetContent side={"right"}>
-        <SheetHeader>Your cart</SheetHeader>
+      <SheetContent
+        side={"right"}
+        className="flex flex-col gap-[30px] [&>button]:hidden"
+      >
+        <div className="flex justify-between">
+          <Badge
+            variant={"outline"}
+            className="flex w-fit flex-row items-center justify-center gap-1 border-2 border-primary py-1"
+          >
+            <ShoppingCartIcon />
+
+            <p className="px-2 font-semibold uppercase">Cart</p>
+          </Badge>
+
+          <SheetClose asChild>
+            <Button size={"icon"} variant={"ghost"}>
+              <XIcon />
+            </Button>
+          </SheetClose>
+        </div>
+
+        {products.length === 0 ? (
+          <>
+            <div className="flex flex-1 justify-center">
+              <p>Oops! Your cart is empty.</p>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </SheetContent>
     </Sheet>
   );
