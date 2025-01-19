43199f90bfac092ecf99f1f60dd2e025f47d91fe
redig <luizredig@gmail.com>
2025-01-19 01:05:05 -0300

    chore: install stripe and @stripe/stripe-js

--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,9 @@ declare namespace NodeJS {
     DATABASE_URL: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+    STRIPE_SECRET_KEY: string;
+    HOST_URL: string;
+    STRIPE_WEBHOOK_SECRET: string;
   }
 }
